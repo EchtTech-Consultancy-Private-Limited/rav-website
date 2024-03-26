6ba443ba8511c2dff183eaf8d40fc692e2b72182
chahal1997 <mesingh9719@gmail.com>
2024-03-26 13:01:41 +0530

    fixed gallery error

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -34,12 +34,14 @@ class GalleryController extends Controller
         $videos = [];
         foreach ($galleryCategories as $item) {
             $categoryImageData = DB::table('gallery_details')->where('gallery_id',$item->uid)->first();
+          if ($categoryImageData) {
             $videos[] = [
                 'uid' => $item->uid,
-                'video_id' => $categoryImageData->public_url,
+                'video_id' => $categoryImageData->public_url ?? '',
                 'title_name_en' => $item->title_name_en,
                 'title_name_hi' => $item->title_name_hi
             ];
+          }
         }
         
         return view('pages.videoGallery',compact('videos'));
