d51d7f37ad5d8dfa2e70a854646854140ae27087
neeraj7355 <neeraj.chaursiya@echttech.com>
2024-04-10 16:50:20 +0530

    prechanges

--- resources/views/partials/footer-script.blade.php
+++ resources/views/partials/footer-script.blade.php
@@ -8,6 +8,8 @@
 <script src="{{ asset('assets/js/custom.js') }}"></script>
 <script src="{{ asset('assets/js/sidebar.js') }}"></script>
 <script src="{{ asset('assets/js/cssjs.js') }}"></script>
+
+
 <script src="{{ asset('assets/js/photo-gallery.js') }}"></script>
 <script src="{{ asset('assets/js/jquery.magnific-popup.min.js') }}"></script>
 <script type="text/javascript" charset="utf8" src="{{ asset('assets/js/jquery.dataTables.min.js') }}"></script>
