be5ab8333c1f767e49118de6741817150c7d24a7
brijesh-echttech <brijesh.sharma@echttech.com>
2024-05-24 13:07:59 +0530

    bug fix

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -142,7 +142,11 @@
         <a href="{{$social_links->twitter}}" class="Twitter"
             onclick="return confirm('@if(Session::get('locale') == 'hi'){{ 'यह लिंक आपको एक बाहरी वेबसाइट पर ले जाएगा.' }}@else{{ 'This link will take you to an external website.' }}@endif ')"
             target="_blank" title="Twitter">
-            <div class="twitter-icon"> <img src="{{ asset('assets/images/twitter_icon.png') }}" alt="twitter" title="twitter"></div>
+            <i>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
+                    <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
+                </svg>
+            </i> 
             @if (Session::get('locale') == 'hi') {{ 'ट्विटर' }}@else{{'Twitter'}}@endif
         </a>
     @endif
